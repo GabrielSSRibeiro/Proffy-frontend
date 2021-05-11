c8d76f53008b369bb0d935facd58fc64a89c9c41
Gabriel Ribeiro <49738156+Gab1235@users.noreply.github.com>
2021-05-10 20:02:11 -0400

    finished landing page

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,13 @@ import React from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 // import api from "../services/api";
 
-import Home from "./pages/Home";
+import Landing from "./pages/Landing";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact component={Landing} />
         <Route render={() => <Redirect to="/" />} />
       </Switch>
     </BrowserRouter>
